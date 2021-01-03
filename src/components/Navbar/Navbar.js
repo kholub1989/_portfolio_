8c05f29b05c1cc5d3f61b0493392f288a4151104
Krisztian Holub <kholub1989@gmail.com>
2021-01-02 20:54:07 -0500

    add navbar commponent

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,160 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Link } from "react-scroll";
+import Logo from "../Logo/Logo";
+import "../../main.scss";
+import { gsap, Power3 } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+const MobileList = () => {
+  const [active, setActive] = useState(false);
+  return (
+    <>
+      {active && <List />}
+      <div
+        className={!active ? "burger-menu" : "burger-menu menu-on"}
+        onClick={() => {
+          setActive(!active);
+        }}
+      >
+        <div className="burger"></div>
+      </div>
+    </>
+  );
+};
+
+const List = () => {
+  return (
+    <nav className="nav">
+      <ul className="nav__list">
+        <li className="nav__item">
+          <Link
+            className="nav__link"
+            activeClass="nav-active"
+            to="home"
+            spy={true}
+            smooth={true}
+            // offset={-50}
+            duration={600}
+          >
+            Home
+          </Link>
+        </li>
+        <li className="nav__item">
+          <Link
+            className="nav__link"
+            activeClass="nav-active"
+            to="about-wrapper"
+            spy={true}
+            smooth={true}
+            // offset={-80}
+            duration={600}
+          >
+            About
+          </Link>
+        </li>
+        <li className="nav__item">
+          <Link
+            className="nav__link"
+            activeClass="nav-active"
+            to="project-wrapper"
+            spy={true}
+            smooth={true}
+            // offset={-70}
+            // offset={-80}
+            duration={600}
+          >
+            Projects
+          </Link>
+        </li>
+        <li className="nav__item">
+          <Link
+            className="nav__link"
+            activeClass="nav-active"
+            to="contact-me-wrapper"
+            spy={true}
+            smooth={true}
+            // offset={-50}
+            duration={600}
+          >
+            Contact me
+          </Link>
+        </li>
+        {/* <li className="nav__item hover-target">
+          <a href="#home" className="smoothscroll nav__link nav-active">
+            Home
+          </a>
+        </li>
+        <li className="nav__item hover-target">
+          <a href="#about" className=" smoothscroll nav__link">
+            About
+          </a>
+        </li>
+        <li className="nav__item hover-target">
+          <a href="#work" className="smoothscroll nav__link">
+            Work
+          </a>
+        </li>
+        <li className="nav__item hover-target">
+          <a href="#contact-me" className="smoothscroll nav__link">
+            Contact Me
+          </a>
+        </li> */}
+      </ul>
+    </nav>
+  );
+};
+
+const Navbar = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  const header = useRef(null);
+  const scroll = useRef(null);
+  const [isMobile, setIsMobile] = useState(
+    window.matchMedia("(max-width:900px)").matches
+  );
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setIsMobile(window.matchMedia("(max-width:900px)").matches);
+    });
+    gsap.from(header.current, {
+      opacity: 0,
+      y: -50,
+      duration: 1.5,
+      ease: Power3.easeOut,
+    });
+    gsap.to("progress", {
+      value: 100,
+      ease: "none",
+      scrollTrigger: { scrub: 0.3 },
+    });
+    // gsap.from("nav", {
+    //   opacity: 0,
+    //   y: -50,
+    //   delay: 0.3,
+    //   duration: 1.5,
+    //   ease: Power3.easeOut,
+    // });
+  }, [header]);
+
+  return (
+    <>
+      <div className="header" ref={header}>
+        <div className="header__wrapper">
+          <Link
+            to="home"
+            smooth={true}
+            offset={-50}
+            duration={400}
+            className="header__wrapper--link"
+          >
+            <Logo />
+          </Link>
+          {isMobile ? <MobileList /> : <List />}
+        </div>
+        <progress max="100" value="0" ref={scroll}></progress>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
