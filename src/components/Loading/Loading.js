a69de0b24412d5a0cb8d4ca1adb1026e0cedc3ab
Krisztian Holub <kholub1989@gmail.com>
2021-01-03 22:02:53 -0500

    add slow loading functionality commponen

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../../main.scss";
+
+function Loading(loading) {
+  return (
+    <div className="loading-container">
+      <div className="loader-div">
+        <div className="loader-div">
+          <div className="loader-div">
+            <div className="loader-div">
+              <div className="loader-div">
+                <div className="loader-div">
+                  <div className="loader-div"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Loading;
