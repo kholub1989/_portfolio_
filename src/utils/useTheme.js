a7accd75598615700be03df205943f7005a748b1
kholub1989 <kholub1989@gmail.com>
2023-05-07 18:38:44 -0400

    refactor dark Theme

--- src/utils/useTheme.js
+++ src/utils/useTheme.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import useDarkMode from "use-dark-mode";
 
 const lightTheme = "light-mode";
 const darkTheme = "dark-mode";
 
 export const useTheme = () => {
-  const darkMode = useDarkMode();
+  const darkMode = useDarkMode(false);
 
   const [theme, setTheme] = React.useState(darkTheme);
 
-  React.useEffect(() => {
+  useEffect(() => {
     setTheme(darkMode?.value ? darkTheme : lightTheme);
   }, [darkMode.value]);
 
